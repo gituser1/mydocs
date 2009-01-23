633a70220bfd8d26fdd252e2552ab46d56b0daf6
prathibha <pratibadominic@yahoo.co.in>
2009-01-23 14:07:06 +0530

    main.c second commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	char name[255];
+
+	printf("Enter your name: ");
+	fgets(name, 255, stdin);
+	printf("length = %d\n", strlen(name)); 
+	name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+  printf("hi testbranch\n");
+	printf("Hello %s!\n", name);
+	return 0;
+}
